d366c7d22ec939fc3bb4cbf9ad2ded23cd93d591
joseph gitau <josephgitau6703@gmail.com>
2023-09-12 02:57:32 -0700

    task 1

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
+"""returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function should return None
 """
-recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit
-"""
+
 import requests
 
+def recurse(subreddit, hot_list=[],  next_page=None, count=0):
+    """Request subreddit recursively using pagination
+    """
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-josephgitau'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    # if page specified, pass as parameter
+    if next_page:
+        url += '?after={}'.format(next_page)
+    headers = {'User-Agent': user_agent}
 
-def recurse(subreddit, hot_list=[], count=0, next_page=None):
-    """return list containing titles of all hot articles"""
-    headers = {
-        "User-Agent": "0x16. API_advanced-e_kiminza"
-    }
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    params = {"limit": 50, "next_page": next_page, "count": count}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
-    if response.status_code != 200:
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
         return None
-    response_ = response.json().get("data")
-    next_page = response_.get("next_page")
-    count += response_.get("dist")
-    children = response_.get("children")
-    for child in children:
-        title = child.get("data").get("title")
-        hot_list.append(title)
+
+    # load response unit from json
+    data = r.json()['data']
+
+    # extract list of pages
+    posts = data['children']
+    for post in posts:
+        count += 1
+        hot_list.append(post['data']['title'])
+
+    next_page = data['after']
     if next_page is not None:
-        return recurse(subreddit, hot_list, count, next_page)
-    return hot_list
+        return recurse(subreddit, hot_list, next_page, count)
+    else:
+        return hot_list
