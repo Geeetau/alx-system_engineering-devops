638acff82905cccc2cf7cf7aabffd19f9f761f61
Joseph Gitau <josephgitau6703@gmail.com>
2023-09-23 09:49:37 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,7 +0,0 @@
-# Increases the amount of traffic Nginx server handle
-
-exec { 'fix--for-nginx':
-  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
-/etc/default/nginx; service nginx restart\"",
-  path    => '/usr/bin:/usr/sbin:/bin'
-}
